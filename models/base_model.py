1a95f6ff62239ce6fdc97c10e3420e4cdb4e20d2
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-09 10:37:30 +0300

    fix pystyle

--- models/base_model.py
+++ models/base_model.py
@@ -19,18 +19,18 @@ class BaseModel:
     def __str__(self):
         """ It returns human readable format of the instance attributes"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                      self.__dict__)
+                                     self.__dict__)
 
     def save(self):
         """It saves instance and update updated_at attribute"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Converts the instance to a dictionary excluding private attrbutes
+        """Converts the instance to a dictionary excluding private attributes
             and convert datetime to string
         """
         full_dict = self.__dict__
         full_dict['__class__'] = self.__class__.__name__
         full_dict['created_at'] = datetime.isoformat(self.created_at)
-        full_dict['updated_at'] = datetime.isoformat(self.updated_at)        
+        full_dict['updated_at'] = datetime.isoformat(self.updated_at)
         return full_dict

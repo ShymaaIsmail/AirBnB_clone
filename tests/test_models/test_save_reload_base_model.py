5b97fc71dce6710dd66eb0bb046e9f67d27d315a
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-10 21:17:51 +0300

    Update test_save_reload_base_model.py

--- tests/test_models/test_save_reload_base_model.py
+++ tests/test_models/test_save_reload_base_model.py
@@ -1,10 +1,36 @@
-#!/usr/bin/python3s
-import unittest
+#!/usr/bin/python3
+import json
+import os
+""" File Storage Module"""
 
-class TestSaveBaseModelModel(unittest.TestCase):
-    pass
 
-print("=========================================")
+class FileStorage:
+    """File Storage Class Definition"""
+    __file_path = "file.json"
+    __objects = {}
 
-class TestReloadBaseModelModel(unittest.TestCase):
-    pass
+    def all(self):
+        """Returns All Stored Objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Set obj with its key in __objects"""
+        if obj:
+            class_name = obj["__class__"]
+            id = obj["id"]
+            self.__objects[f"{class_name}.{id}"] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        with open(self.__file_path, "w") as file:
+            json.dump(self.__objects, file)
+            self.reload()
+
+    def reload(self):
+        """Deserializes the JSON file to __objects if __file_path exists"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                try:
+                    self.__objects = json.load(file)
+                except:
+                    self.__objects = {}

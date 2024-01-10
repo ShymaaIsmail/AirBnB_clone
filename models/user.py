3c6cd15a869e669d88699de1bde05de4b145a1e6
3zzazakl <vanzekovichgr@gmail.com>
2024-01-10 15:09:57 +0200

    adding atrributes about user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Setting User info"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

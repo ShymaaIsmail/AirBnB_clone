7705d1158b273694905f0ba407e030bc5f92cd3c
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-10 22:56:57 +0300

    Task 5 build file storage (#8)

--- tests/test_models/test_save_reload_base_model.py
+++ tests/test_models/test_save_reload_base_model.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3s
 import unittest
+from models import storage
+from models.base_model import BaseModel
 
-class TestSaveBaseModelModel(unittest.TestCase):
-    pass
 
-print("=========================================")
+class TestSaveReloadBaseModel(unittest.TestCase):
 
-class TestReloadBaseModelModel(unittest.TestCase):
-    pass
+    def test_default(self):
+        all_objs = storage.all()
+        print("-- Reloaded objects --")
+        for obj_id in all_objs.keys():
+            obj = all_objs[obj_id]
+            print(obj)
+        print("-- Create a new object --")
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        my_model.save()
+        print(my_model)

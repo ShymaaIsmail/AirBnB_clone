d2cc7471dbfcd1281591d1b0cc57729a0d6af2b5
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-01-10 21:11:03 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
 """ Models package initialization"""
 from .base_model import BaseModel
+from .engine.file_storage import  FileStorage
+storage = FileStorage()
+storage.reload()
 
-__all__ = [BaseModel]
+__all__ = [BaseModel, storage]

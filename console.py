3fff915c511cbf69edeb32085cf2f757c183866f
Azzaz Akl <140283548+3zzazakl@users.noreply.github.com>
2024-01-12 07:22:45 +0200

    solved non-interactive mode and help commands for each command (#13)

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 
 
 import cmd
+import sys
+from models import storage
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -12,6 +14,7 @@ from models.state import State
 from models.user import User
 
 classes = ["BaseModel", "Amenity", "City", "Place", "Review", "State", "User"]
+objects = storage.all()
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +26,7 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_EOF(self, arg):
+        """CTRL_C to Exit"""
         return True
 
     def emptyline(self):
@@ -31,53 +35,93 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creating instance of class"""
-        args = arg.split()
-        if len(args) == 0:
+        if not arg:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
+        else:
+            try:
+                instant_id = eval(arg)()
+                instant_id.save()
+                print(instant_id.id)
+            except NameError:
+                print("** class doesn't exist **")
 
     def do_show(self, arg):
         """Showing instance of class"""
         args = arg.split()
-        if len(args) == 0:
+        if not arg:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
+        else:
+            class_name = args[0]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                instant_id = args[1]
+                key = "{}.{}".format(class_name, instant_id)
+                if key in objects:
+                    print(objects[key])
+                else:
+                    print("** no instance found **")
 
     def do_destroy(self, arg):
         """Destroy instance of class"""
         args = arg.split()
-        if len(args) == 0:
+        if not args:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
+        else:
+            class_name = args[0]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                instant_id = args[1]
+                key = "{}.{}".format(class_name, instant_id)
+                if key in objects:
+                    del objects[key]
+                    storage.save()
+                else:
+                    print("** no instance found **")
 
     def do_all(self, arg):
         """Printing all instance of class"""
-        args = arg.split()
-        if len(args) == 0:
-            print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
+        if not arg:
+            print([str(value) for value in objects.values()])
+        else:
+            class_name = arg.split()[0]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+            else:
+                filtered_objects = [str(value) for key, value in objects.items() if key.split(".")[0] == class_name]
+                print(filtered_objects)
 
     def do_update(self, arg):
         """Updating instance of class"""
         args = arg.split()
-        if len(args) == 0:
+        if not args:
             print("** class name missing **")
-        elif args[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        elif len(args) == 2:
-            print("** attribute name missing **")
-        elif len(args) == 3:
-            print("** value missing **")
+        else:
+            class_name = args[0]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                instant_id = args[1]
+                key = "{}.{}".format(class_name, instant_id)
+                if key not in objects:
+                    print("** no instance found **")
+                elif len(args) < 3:
+                    print("** attribute name missing **")
+                elif len(args) < 4:
+                    print("** value missing **")
+                else:
+                    attribute_name = args[2]
+                    attribute_value = args[3]
+                    instant = objects[key]
+                    setattr(instant, attribute_name, attribute_value)
+                    instant.save()
 
     def default(self, arg):
         print(f"Unknown command: {arg}")
@@ -91,6 +135,38 @@ class HBNBCommand(cmd.Cmd):
         """help for EOF"""
         print()
 
+        """commands: update
+        """
+    def help_create():
+        """Create command creating a new instance"""
+        print("Help command to create a new instance")
+        print()
+
+    def help_show():
+        """Show command for Showing all attributes about instance"""
+        print("Show command to display all attributes about instance")
+        print()
+
+    def help_destroy():
+        """Destroy command for Deleting an instance"""
+        print("Destroy command to delete an instance")
+        print()
+
+    def help_all():
+        """All command to Display all objects"""
+        print("All command to Displaying all objects")
+        print()
+
+    def help_update():
+        """Update command to modify instance attributes"""
+        print("Update command to modify instance attributes")
+        print()
+
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            HBNBCommand().onecmd(arg)
+
+    else:
+        HBNBCommand().cmdloop()

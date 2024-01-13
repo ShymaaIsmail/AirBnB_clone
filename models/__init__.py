e223eec88fd956c6f9612c0e65bffaeec1c8275a
shymaaismail <shymaa.m.ismail@gmail.com>
2024-01-13 12:51:14 +0300

    remove __all__

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,4 @@
 """ Models package initialization"""
-from .base_model import BaseModel
 from .engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
-
-__all__ = [BaseModel, storage]
